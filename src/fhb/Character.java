471b826f50e4024f1f1e990c4b62b37f7975fae2
valekll <45467718+valekll@users.noreply.github.com>
2019-02-09 11:33:19 -0500

    Sprite doneish

--- src/fhb/Character.java
+++ src/fhb/Character.java
@@ -1,11 +1,21 @@
 package fhb;
 
+import javafx.scene.image.Image;
+
 public class Character {
 	private int hp;
-	private String dia;
 	private String name;
+	private String dia;
 	private Sprite sprite;
 	
+	public Character(int h, String n, String d, double x, double y, Image[] normal, Image[] build, Image[] death) {
+		hp = h;
+		name = n;
+		dia = d;
+		sprite = new Sprite(x, y, normal, build, death);
+	} //Character()
+	
+	//TODO:
 	public boolean decHp(int dmg) {
 		if(true) {
 			return true;
--- src/fhb/Sprite.java
+++ src/fhb/Sprite.java
@@ -8,32 +8,111 @@ public class Sprite {
 	private double yax;
 	private Image[] nml; //normal
 	private ImageView[] nmlv;
+	private int nmlct;
 	private Image[] bd; //build
 	private ImageView[] bdv;
+	private int bdct;
 	private Image[] dth; //death
 	private ImageView[] dthv;
+	private int dthct;
 	
-	//TODO:
-	public boolean cycle() {
-		if(true) {
-			return true;
+	public Sprite(double x, double y,Image[] normal, Image[] build, Image[] death) {
+		xax = x;
+		yax = y;
+		nml = normal;
+		nmlv = imageToImageViewArr(nml);
+		bd = build;
+		bdv = imageToImageViewArr(bd);
+		dth = death;
+		dthv = imageToImageViewArr(dth);
+	} //Sprite()
+	
+	/**
+	 * Change arr of Image to ImageView
+	 * @param img the Image array
+	 * @return ImageView Array
+	 */
+	public static ImageView[] imageToImageViewArr(Image ... img) {
+		ImageView[] imgv = new ImageView[img.length];
+		for(int i = 0; i > img.length; i++) {
+			imgv[i] = new ImageView(img[i]);
+		} //for
+		return imgv;
+	} //imageToImageViewArr()
+	
+	/**
+	 * Cycles to next ImageView sprite
+	 * @return the next ImageView
+	 */
+	public ImageView cycle() {
+		nmlct++;
+		if(nmlct >= nmlv.length) {
+			nmlct = 0;
 		} //if
-		return false;
+		ImageView img = nmlv[nmlct];
+		return img;
 	} //cycle()
 	
-	//TODO:
-	public boolean build() {
-		if(true) {
-			return true;
+	/**
+	 * @return number of frames
+	 */
+	public int getCycleLength() {
+		return nmlv.length;
+	} //getCycleLength()
+	
+	/**
+	 * Build animation
+	 * @return the next ImageView for build
+	 */
+	public ImageView build() {
+		bdct++;
+		if(bdct >= bdv.length) {
+			bdct = 0;
 		} //if
-		return false;
+		ImageView img = bdv[bdct];
+		return img;
 	} //build()
 	
-	//TODO:
-	public boolean death() {
-		if(true) {
-			return true;
+	/**
+	 * @return number of frames
+	 */
+	public int getBuildLength() {
+		return bdv.length;
+	} //getBuildLength()
+	
+	/**
+	 * Death animation
+	 * @return the next ImageView for death
+	 */
+	public ImageView death() {
+		dthct++;
+		if(dthct >= dthv.length) {
+			dthct = 0;
 		} //if
-		return false;
+		ImageView img = dthv[dthct];
+		return img;
 	} //death()
+	
+	/**
+	 * @return number of frames
+	 */
+	public int getDeathLength() {
+		return dthv.length;
+	} //getDeathLength()
+
+	public double getXax() {
+		return xax;
+	} //getXax()
+
+	public void setXax(double xax) {
+		this.xax = xax;
+	} //setXax()
+
+	public double getYax() {
+		return yax;
+	} //getYax()
+
+	public void setYax(double yax) {
+		this.yax = yax;
+	} //setYax()
 } //Sprite
