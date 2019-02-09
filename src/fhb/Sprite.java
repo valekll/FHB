43f4a4ffa32fba5df622eb57ba4852d79745d072
valekll <45467718+valekll@users.noreply.github.com>
2019-02-09 00:01:12 -0500

    Create Sprite.java

---
+++ src/fhb/Sprite.java
@@ -0,0 +1,13 @@
+package fhb;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Sprite {
+	boolean cycle() {
+		if(true) {
+			return true;
+		} //if
+		return false;
+	} //cycle()
+} //Sprite
