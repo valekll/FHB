7f7e06e435d1bf91175ee0fafa1b641104894b5b
valekll <45467718+valekll@users.noreply.github.com>
2019-02-09 12:01:20 -0500

    Character and Building updated

--- src/fhb/Building.java
+++ src/fhb/Building.java
@@ -1,5 +1,11 @@
 package fhb;
 
+import javafx.scene.image.Image;
+
 public class Building extends Character {
+
+	public Building(int h, String n, String d, double x, double y, Image[] normal, Image[] build, Image[] death) {
+		super(h, n, d, x, y, normal, build, death);
+	} //Building()
 	
 } //Building
--- src/fhb/Character.java
+++ src/fhb/Character.java
@@ -4,22 +4,46 @@ import javafx.scene.image.Image;
 
 public class Character {
 	private int hp;
+	private int hpc;
 	private String name;
 	private String dia;
 	private Sprite sprite;
 	
 	public Character(int h, String n, String d, double x, double y, Image[] normal, Image[] build, Image[] death) {
 		hp = h;
+		hpc = hp;
 		name = n;
 		dia = d;
 		sprite = new Sprite(x, y, normal, build, death);
 	} //Character()
 	
-	//TODO:
+	public void resetHp() {
+		hpc = hp;
+	} //resetHp()
+	
+	/**
+	 * @param dmg damage to deal
+	 * @return 
+	 */
 	public boolean decHp(int dmg) {
-		if(true) {
+		if(dmg > 0) {
+			hpc -= dmg;
 			return true;
 		} //if
 		return false;
 	} //decHp()
+	
+	/**
+	 * Checks for death and runs the death sequence.
+	 * @return false if alive, true if dead
+	 */
+	public boolean checkDead() {
+		if(hpc > 0) {
+			return false;
+		} //if
+		/*
+		 * Run death sequence here
+		 */
+		return true;
+	} //checkDead()
 } //Character
